2a4434b326c6dd0d062506e7773ce4a2f696fbc9
Ron Liao <rliao73@gmail.com>
2025-10-11 19:32:05 -0400

    Initial Commit

---
+++ PH_ATM_Simulator/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  PH_ATM_Simulator
+//
+//  Created by Rong Liao on 2025-10-11.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
